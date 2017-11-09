70f1fb9c33d82bcd32c05f443ebd82bf96c24682
Flavien <fkoehler@student.42.fr>
2017-11-09 19:30:09 +0100

    add memory address overflow protection

--- src/nm/arch_64.c
+++ src/nm/arch_64.c
@@ -6,13 +6,26 @@
 /*   By: fkoehler <fkoehler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/08 14:15:30 by fkoehler          #+#    #+#             */
-/*   Updated: 2017/11/09 16:41:45 by fkoehler         ###   ########.fr       */
+/*   Updated: 2017/11/09 19:26:35 by fkoehler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "nm_otool.h"
+/* WIP
+static int	nm_syms_type(t_nm_otool *env)
+{
+	struct segment_command	*sc;
 
-static void	nm_output_64(t_nm_otool *env, struct symtab_command *symtab_command)
+	sc = env->file_start +
+	sizeof(struct mach_header_64) + sizeof(struct load_command);
+	if ((void*)sc > env->file_end)
+		return (put_error(MALFORMED, env->exec, env->file_name));
+	ft_put_uintmax(sc->nsects, 10);
+	return (0);
+}
+WIP */
+static int	nm_syms_infos_64(t_nm_otool *env,
+struct symtab_command *symtab_command)
 {
 	int				i;
 	int				nsyms;
@@ -23,12 +36,25 @@ static void	nm_output_64(t_nm_otool *env, struct symtab_command *symtab_command)
 	nsyms = (int)symtab_command->nsyms;
 	stringtable = env->file_start + symtab_command->stroff;
 	symtable = env->file_start + symtab_command->symoff;
+	if (stringtable > env->file_end || (void*)symtable > env->file_end)
+		return (put_error(MALFORMED, env->exec, env->file_name));
 	ascii_sort_64(stringtable, symtable, nsyms);
+	nm_syms_type(env);
 	while (i < nsyms)
 	{
-		ft_putendl(stringtable + symtable[i].n_un.n_strx);
+		if (!symtable[i].n_value)
+		{
+			ft_printf("%16c %s\n", ' ',
+			(char*)stringtable + symtable[i].n_un.n_strx);
+		}
+		else
+		{
+			ft_printf("%016llx %s\n", symtable[i].n_value,
+			(char*)stringtable + symtable[i].n_un.n_strx);
+		}
 		i++;
 	}
+	return (0);
 }
 
 int			nm_handle_64(t_nm_otool *env)
@@ -42,13 +68,13 @@ int			nm_handle_64(t_nm_otool *env)
 	header = (struct mach_header_64*)env->file_start;
 	ncmds = header->ncmds;
 	lc = env->file_start + sizeof(*header);
-	if ((void*)lc > env->file_end)
-		return (put_error(MALFORMED, env->exec, env->file_name));
 	while (i < ncmds)
 	{
+		if ((void*)lc > env->file_end)
+			return (put_error(MALFORMED, env->exec, env->file_name));
 		if (lc->cmd == LC_SYMTAB)
 		{
-			nm_output_64(env, (struct symtab_command*)lc);
+			nm_syms_infos_64(env, (struct symtab_command*)lc);
 			break;
 		}
 		i++;
