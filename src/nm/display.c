6fc3f0cd9d9b30168a2e69f5e2361cb2c7e67c03
Flavien <fkoehler@student.42.fr>
2017-11-17 15:40:56 +0100

    fix symbol value display

--- src/nm/display.c
+++ src/nm/display.c
@@ -6,7 +6,7 @@
 /*   By: fkoehler <fkoehler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/13 19:10:45 by fkoehler          #+#    #+#             */
-/*   Updated: 2017/11/15 11:40:10 by fkoehler         ###   ########.fr       */
+/*   Updated: 2017/11/17 15:40:01 by fkoehler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 static void	print_sym_32(uint32_t value, char type, char *str)
 {
-	if (!value)
+	if (!value && (type == 'u' || type == 'U'))
 		ft_printf("%8c %c %s\n", ' ', type, str);
 	else
 		ft_printf("%08llx %c %s\n", value, type, str);
@@ -22,7 +22,7 @@ static void	print_sym_32(uint32_t value, char type, char *str)
 
 static void	print_sym_64(uint64_t value, char type, char *str)
 {
-	if (!value)
+	if (!value && (type == 'u' || type == 'U'))
 		ft_printf("%16c %c %s\n", ' ', type, str);
 	else
 		ft_printf("%016llx %c %s\n", value, type, str);
