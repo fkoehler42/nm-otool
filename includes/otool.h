69331af23e5f876b50bf98716d7d601d3f286849
Flavien <fkoehler@student.42.fr>
2017-11-29 12:42:40 +0100

    otool arch_32 display ok

--- includes/otool.h
+++ includes/otool.h
@@ -6,7 +6,7 @@
 /*   By: fkoehler <fkoehler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/23 14:45:58 by fkoehler          #+#    #+#             */
-/*   Updated: 2017/11/29 12:16:03 by fkoehler         ###   ########.fr       */
+/*   Updated: 2017/11/29 12:35:45 by fkoehler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,9 @@ int					handle_lib(t_otool *env);
 
 int					init_env_struct(t_otool *env);
 
-void				print_section_64(char *sec_name, uint64_t sec_addr,
+void				print_section_32(char *sec_name, uint32_t sec_addr,
 					uint32_t sec_size, uint8_t *content);
+void				print_section_64(char *sec_name, uint64_t sec_addr,
+					uint64_t sec_size, uint8_t *content);
 
 #endif
--- src/otool/display.c
+++ src/otool/display.c
@@ -6,7 +6,7 @@
 /*   By: fkoehler <fkoehler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/23 18:32:44 by fkoehler          #+#    #+#             */
-/*   Updated: 2017/11/29 12:22:54 by fkoehler         ###   ########.fr       */
+/*   Updated: 2017/11/29 12:39:49 by fkoehler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,34 @@ static void	print_byte_value(uint8_t byte)
 	}
 }
 
-void		print_section_64(char *sec_name, uint64_t sec_addr,
+void		print_section_32(char *sec_name, uint32_t sec_addr,
 uint32_t sec_size, uint8_t *content)
 {
 	uint32_t	i;
+
+	i = 0;
+	ft_printf("Contents of (%s,", ft_strupper(sec_name));
+	ft_printf("%s) section\n", ft_strlower(sec_name));
+	while (i < sec_size)
+	{
+		if (i == 0 || i % 16 == 0)
+		{
+			if (i != 0)
+				sec_addr += 16;
+			ft_printf("%08lx\t", sec_addr);
+		}
+		print_byte_value(*content);
+		if ((++i % 16) == 0 && i < sec_size)
+			ft_putchar('\n');
+		content++;
+	}
+	ft_putchar('\n');
+}
+
+void		print_section_64(char *sec_name, uint64_t sec_addr,
+uint64_t sec_size, uint8_t *content)
+{
+	uint64_t	i;
 
 	i = 0;
 	ft_printf("Contents of (%s,", ft_strupper(sec_name));
